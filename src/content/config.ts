d1f3dffc5b3597b81724ba3de3e6dfa5dc5702bf
Nithin-Anand <nithin@hotmail.co.uk>
2025-04-08 22:56:55 +0100

    Begin content collection development

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const albums = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      cover: image(),
+    }),
+});
+
+// const blog = defineCollection({
+//     type: ""
+// })
+
+export const collections = {
+  albums,
+};
